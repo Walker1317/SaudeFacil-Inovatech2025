4e87727ab1f37b71c866080d5868e17ce996066a
Walker Mayklin Mauricio Silva <walkermauricio17@gmail.com>
2025-10-29 03:35:49 -0400

    CRM-7: Definição de Tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:saude_facil_inovatech/widgets/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: appTheme(),
       home: Scaffold(),
     );
   }
---
+++ lib/widgets/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme(){
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1FAE91)),
+  );
+}
